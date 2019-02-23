bd786c965cffd949696d9c012fa33f04b62aab08
borschetsky <wod.moshkin@gmail.com>
2019-02-24 00:19:45 +0200

    Last query were rewrited with joins

--- 07-morejoins.sql
+++ 07-morejoins.sql
@@ -55,7 +55,8 @@ FROM movie m JOIN casting c ON(m.id = c.movieid)
 JOIN actor a ON (c.actorid = a.id)
 WHERE m.yr = 1962 AND c.ord = 1;
 
-##11
+#11
+#Which were the busiest years for 'John Travolta', show the year and the number of movies he made each year for any year in which he made more than 2 movies.
 SELECT yr,COUNT(title) 
 FROM movie JOIN casting ON movie.id=movieid
 JOIN actor ON actorid=actor.id
@@ -67,14 +68,16 @@ HAVING COUNT(title)=(SELECT MAX(c) FROM
          JOIN actor   ON actorid=actor.id
  where name='John Travolta'
  GROUP BY yr) AS t		
-)
+);
 
 #12
+#List the film title and the leading actor for all of the films 'Julie Andrews' played in.
 SELECT m.title, a.name FROM movie m 
 JOIN casting c ON(m.id = c.movieid AND c.ord = 1)
 JOIN actor a ON(c.actorid = a.id)
 WHERE m.id IN(SELECT movieid FROM casting WHERE actorid IN(SELECT id FROM actor WHERE name = 'Julie Andrews') );
 
+#One Nested Query with Joins            
 SELECT m.title, a.name FROM movie m 
 JOIN casting c ON(m.id = c.movieid AND c.ord = 1)
 JOIN actor a ON(c.actorid = a.id)
@@ -84,6 +87,7 @@ JOIN actor a ON (a.id = c.actorid)
 WHERE a.name = 'Julie Andrews' );
 
 #13
+#Obtain a list, in alphabetical order, of actors who've had at least 30 starring roles.
 SELECT a.name
  FROM casting c JOIN actor a ON(c.actorid = a.id AND c.ord = 1)
 GROUP BY a.name
@@ -99,10 +103,18 @@ GROUP BY m.title, m.id
 ORDER BY quantity DESC, m.title;
 
 #15
+#List all the people who have worked with 'Art Garfunkel'.
+#Nested query :) 
 SELECT name FROM actor 
 WHERE actor.id IN
-(SELECT actorid FROM casting c WHERE c.movieid IN
-	(SELECT c.movieid FROM casting c WHERE c.actorid = 
-		(SELECT a.id FROM actor a WHERE a.name = 'Art Garfunkel')) 
+(SELECT actorid FROM casting c 
+	WHERE c.movieid IN (SELECT c.movieid FROM casting c WHERE c.actorid = (SELECT a.id FROM actor a WHERE a.name = 'Art Garfunkel')) 
 			AND c.actorid != (SELECT a.id FROM actor a WHERE a.name = 'Art Garfunkel'));
+
+#One Nested Query with Joins            
+SELECT a.name FROM actor a JOIN casting c ON(c.actorid = a.id)
+WHERE c.movieid IN (SELECT m.id FROM movie m 
+JOIN casting c ON(m.id = c.movieid)
+JOIN actor a ON(c.actorid = a.id)
+WHERE a.name = 'Art Garfunkel') AND a.name != 'Art Garfunkel';      
 
